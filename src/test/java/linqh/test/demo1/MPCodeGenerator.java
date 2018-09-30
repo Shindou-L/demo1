3048611f00315a0150e14af552bf1badc44f4c4e
Shindou-L <631867231@qq.com>
2018-09-30 10:59:35 +0800

    代码生成器配置

---
+++ src/test/java/linqh/test/demo1/MPCodeGenerator.java
@@ -0,0 +1,96 @@
+package linqh.test.demo1;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+public class MPCodeGenerator {
+
+    private static String dirPath = "D:\\mp\\ms";
+
+    public static void main(String[] main) throws Exception {
+        AutoGenerator mpg = new AutoGenerator();
+        // 选择 freemarker 引擎，默认 Veloctiy
+        //mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(dirPath);
+        gc.setAuthor("lqh");
+        gc.setFileOverride(true); //是否覆盖
+        gc.setActiveRecord(true);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(true);// XML columList
+        gc.setServiceImplName("%sService");
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        // gc.setMapperName("%sDao");
+        // gc.setXmlName("%sMapper");
+        // gc.setServiceName("MP%sService");
+        // gc.setServiceImplName("%sServiceDiy");
+        // gc.setControllerName("%sAction");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/msdb?characterEncoding=utf8");
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setEntityLombokModel(false);
+        strategy.setRestControllerStyle(true);
+        // strategy.setInclude(new String[] { "user" }); // 需要生成的表
+        // strategy.setExclude(new String[]{"test"}); // 排除生成的表
+        // 自定义实体父类
+        // strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
+        // 自定义实体，公共字段
+        // strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
+        // 自定义 mapper 父类
+        // strategy.setSuperMapperClass("com.baomidou.demo.TestMapper");
+        // 自定义 service 父类
+        // strategy.setSuperServiceClass("com.baomidou.demo.TestService");
+        // 自定义 service 实现类父类
+        // strategy.setSuperServiceImplClass("com.baomidou.demo.TestServiceImpl");
+        // 自定义 controller 父类
+        // strategy.setSuperControllerClass("com.baomidou.demo.TestController");
+        // 【实体】是否生成字段常量（默认 false）
+        // public static final String ID = "test_id";
+        // strategy.setEntityColumnConstant(true);
+        // 【实体】是否为构建者模型（默认 false）
+        // public User setName(String name) {this.name = name; return this;}
+        strategy.setEntityBuilderModel(true);
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.quantil.ms.api");
+        pc.setModuleName("all");
+        pc.setController("controler");
+        pc.setEntity("domain");
+        pc.setMapper("mapper");
+        pc.setService("serviceinterface");
+        pc.setServiceImpl("service");
+        pc.setXml("mapper");
+        mpg.setPackageInfo(pc);
+
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setController("controller.vm");
+        templateConfig.setEntity("domain.vm");
+        templateConfig.setMapper("mapper.vm");
+        templateConfig.setXml("mapperxml.vm");
+        templateConfig.setService(null);
+        templateConfig.setServiceImpl("serviceimpl.vm");
+        mpg.setTemplate(templateConfig);
+
+        // 执行生成
+        mpg.execute();
+    }
+}
